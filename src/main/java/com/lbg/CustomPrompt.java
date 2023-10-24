a523485f1849b441829d9aebf42d5319bbd6f13a
Adam.B3 <readingfootyclub1871@gmail.com>
2023-10-24 16:45:24 +0100

    UI taken from Sel, should be changed to take in the correct inputs

---
+++ src/main/java/com/lbg/CustomPrompt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lbg;
+
+/**
+ *
+ * @author selvy
+ */
+public class CustomPrompt   implements ICustomPrompt
+{
+    @Override
+    public void prompt( String msg )
+    {
+        System.out.print( "[" + msg + "] " );
+    }    
+}
